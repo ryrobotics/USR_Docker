c93ffda787b399706d9cfcf78357f3842d26af8d
Ray0117 <wry0117@outlook.com>
2022-07-11 17:23:41 +0800

    Modify for  D435

--- ROS-melodic-Xavier/dockerun.sh
+++ ROS-melodic-Xavier/dockerun.sh
@@ -6,16 +6,16 @@ xhost +
 
 if [ $1 = 'build' ]
 then
-    docker build -t ryrobotics/ros_melodic_l4t:mavros . -f Dockerfile.l4t
+    docker build -t ryrobotics/ros_melodic_l4t:d435 . -f Dockerfile.l4t
     docker run -it --privileged --net=host \
-         --runtime=nvidia \
+        --runtime=nvidia \
         --env=LOCAL_USER_ID="$(id -u)" \
         --env="DISPLAY" \
         --env="QT_X11_NO_MITSHM=1" \
         --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
         --volume ~/src:/src/:rw \
         --restart=always \
-        --name=nx_mavros ryrobotics/ros_melodic_l4t:mavros bash
+        --name=nx_d435 ryrobotics/ros_melodic_l4t:d435 bash
 
 elif [ $1 = 'build_arm64' ]
 then
