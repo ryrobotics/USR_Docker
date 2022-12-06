efca6e81d9f4f0fa74d643b92cbcf7c2453e9287
Ruoyu Wang <wry0117@outlook.com>
2022-12-06 08:13:28 +0000

    Add support for Khadas VIM3

---
+++ ROS-melodic-vim3/dockerun.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+# enable access to xhost from the container
+xhost +
+
+docker_name='lio'
+
+if [ $1 = 'build' ]
+then
+    docker build -t ryrobotics/ros_melodic_vim3:${docker_name} . -f Dockerfile.vim3
+fi
+
+# Run docker
+if [ $1 = 'run' ]
+then
+    docker run -it --privileged --net=host \
+        --env=LOCAL_USER_ID="$(id -u)" \
+        --env="DISPLAY" \
+        --env="QT_X11_NO_MITSHM=1" \
+        --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        --volume ~/src:/src/:rw \
+        --restart=always \
+        --name=nx_${docker_name} ryrobotics/ros_melodic_vim3:${docker_name} bash
+fi
