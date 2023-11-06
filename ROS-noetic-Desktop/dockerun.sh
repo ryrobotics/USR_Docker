3492cd8f450c04cb40d89853126fc0f1b24d562b
Ruoyu Wang <wry0117@outlook.com>
2023-11-06 22:31:30 +0800

    Update noetic docker for rviz

--- ROS-noetic-Desktop/dockerun.sh
+++ ROS-noetic-Desktop/dockerun.sh
@@ -10,14 +10,15 @@ mkdir -p ~/${version}_src
 
 if [ $1 = 'build' ]
 then
-    # docker build -t ryrobotics/ros_noetic_desktop:mavros . -f Dockerfile.noetic
-    docker run -it --privileged --net=host \
-        --env=LOCAL_USER_ID="$(id -u)" \
-        --env="DISPLAY" \
-        --env="QT_X11_NO_MITSHM=1" \
-        --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
-        --volume ~/noetic_src:/src/:rw \
-        --name=mpc ryrobotics/ros_noetic_desktop:mavros bash
+    docker build -t ryrobotics/ros_noetic_desktop:mavros . -f Dockerfile.noetic
+    # docker run -it --privileged --net=host \
+    #     --runtime nvidia \
+    #     --env=LOCAL_USER_ID="$(id -u)" \
+    #     --env="DISPLAY" \
+    #     --env="QT_X11_NO_MITSHM=1" \
+    #     --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    #     --volume ~/noetic_src:/src/:rw \
+    #     --name=noetic ryrobotics/ros_noetic_desktop:mavros bash
 
 fi
 
